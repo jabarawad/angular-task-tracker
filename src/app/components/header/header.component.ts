722b0e99161cfe941fcdb9972345e51cc0a9c43a
jabarawad <77428257+jabarawad@users.noreply.github.com>
2021-06-21 15:59:55 -0400

    added to db

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,37 +1,34 @@
-
 import { Component, OnInit } from '@angular/core';
-import { UiService } from '../../services/ui.service';
+import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  showAddTask: boolean = false;
+  showAddTask: boolean = true;
   subscription: Subscription;
 
-  constructor(private uiService: UiService, private router: Router) {
-    this.subscription = this.uiService
-      .onToggle()
-      .subscribe((value) => (this.showAddTask = value));
+  constructor(private uiService: UiService, 
+    private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe(
+      (value => this.showAddTask = value)
+    );
   }
 
-  ngOnInit(): void {}
-  
-   ngOnDestroy() {
-     // Unsubscribe to ensure no memory leaks
-     this.subscription.unsubscribe();
-   }
+  ngOnInit(): void {
+  }
 
-  toggleAddTask() {
+  toggleAddTask(){
     this.uiService.toggleAddTask();
   }
 
-  hasRoute(route: string) {
+  hasRoute(route: string){
     return this.router.url === route;
   }
+
 }
