dd4d1da5b6e6121187f3e316ce2ca0513c2314f4
jabarawad <77428257+jabarawad@users.noreply.github.com>
2021-06-21 16:00:54 -0400

    made change to button functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  showAddTask: boolean = true;
+  showAddTask: boolean;
   subscription: Subscription;
 
   constructor(private uiService: UiService, 
